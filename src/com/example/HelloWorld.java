cd0861bf8079028211105127b8eb0f9477495cec
yogesh1842 <yogesh1842@yogesh1842-02.Nagarro.local>
2020-01-29 14:39:12 +0530

    Ayega to modi hi.

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -3,5 +3,6 @@ package com.example;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello java world");
+		System.out.println("Chnages are done");
 	}
 }
